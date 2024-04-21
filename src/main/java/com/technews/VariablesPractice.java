6598a52a4243f68e2dfba7931450af511fdc11a6
mintyry <gitmintyry@gmail.com>
2024-04-20 21:27:05 -0700

    variablespractice

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int two = 2;
+        double progress = 2.2;
+        double sum = two + progress;
+        System.out.println(sum);
+
+        String cat = "dog";
+
+        String result = sum + cat;
+        System.out.println(result);
+    }
+}
