f9a6e16218598010c39a6f0a8cd6112bb3b7ff66
mintyry <gitmintyry@gmail.com>
2024-04-22 19:38:41 -0700

    annotated some variables in user class

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -1,8 +1,7 @@
 package com.technews.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 //persisting object so it can map to a table
 @Entity
@@ -11,4 +10,21 @@ import jakarta.persistence.Table;
 //Name the table
 @Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    
+    @Transient
+    boolean loggedIn;
+
+    private List<Post> posts;
+    private List<Vote> votes;
+    private List<Comment> comments;
 }
