7d7c71d336ee4368a1deaca62319dd9ad7c3db6b
mintyry <gitmintyry@gmail.com>
2024-04-24 19:23:59 -0700

    created Post class, added vars and annotations

---
+++ src/main/java/com/technews/model/Post.java
@@ -0,0 +1,40 @@
+package com.technews.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "post")
+public class Post implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String title;
+
+    private String postUrl;
+
+    @Transient
+    private String userName;
+
+    @Transient
+    private int voteCount;
+
+    private Integer userId;
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name = "posted_at")
+    private Date postedAt = new Date();
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name = "updated_at")
+    private Date updatedAt = new Date();
+    
+    private List<Comment> comments;
+}
