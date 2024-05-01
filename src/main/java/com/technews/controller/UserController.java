0cde363ee18446b70a6a0f6020c9ec0cb3c09d95
mintyry <gitmintyry@gmail.com>
2024-04-30 17:28:40 -0700

    get all users

--- src/main/java/com/technews/controller/UserController.java
+++ src/main/java/com/technews/controller/UserController.java
@@ -1,7 +1,35 @@
 package com.technews.controller;
 
+import com.technews.model.Post;
+import com.technews.model.User;
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
+    @Autowired
+    UserRepository repository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @GetMapping("/api/users")
+    public List<User> getAllUsers() {
+        List<User> userList = repository.findAll();
+        for (User u : userList) {
+            List<Post> postList = u.getPosts();
+            for (Post p : postList) {
+                p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+            }
+        }
+        return userList;
+    }
 }
+
+
+
