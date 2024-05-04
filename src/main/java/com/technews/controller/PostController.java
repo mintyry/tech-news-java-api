6d69fe55a84a49e0621184f14ff00ee1e1383916
mintyry <gitmintyry@gmail.com>
2024-05-03 18:22:20 -0700

    get routes and post route for postcontroller made

--- src/main/java/com/technews/controller/PostController.java
+++ src/main/java/com/technews/controller/PostController.java
@@ -1,7 +1,50 @@
 package com.technews.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.technews.model.Post;
+import com.technews.repository.PostRepository;
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PostController {
+    @Autowired
+    PostRepository repository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+//    get posts
+    @GetMapping("/api/posts")
+    public List<Post> getAllPosts(){
+        List<Post> postList = repository.findAll();
+        for (Post p : postList) {
+            p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+        }
+        return postList;
+    }
+
+//    get single post
+    @GetMapping("/api/posts/{id}")
+    public Post getPost(@PathVariable Integer id) {
+        Post returnPost = repository.getById(id);
+        returnPost.setVoteCount(voteRepository.countVotesByPostId(returnPost.getId()));
+
+        return returnPost;
+    }
+
+//    Create post
+    @PostMapping("/api/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post addPost(@RequestBody Post post) {
+        repository.save(post);
+        return post;
+    }
 }
