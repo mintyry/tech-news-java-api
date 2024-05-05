44405d2149b104333f90bb2d634e3045bb4a3c05
mintyry <gitmintyry@gmail.com>
2024-05-04 22:22:40 -0700

    get routes for comments and single comments comment controller

--- src/main/java/com/technews/controller/CommentController.java
+++ src/main/java/com/technews/controller/CommentController.java
@@ -1,7 +1,32 @@
 package com.technews.controller;
 
+import com.technews.model.Comment;
+import com.technews.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CommentController {
+
+//    make comment methods and such available
+    @Autowired
+    CommentRepository repository;
+
+//    get route for comments
+    @GetMapping("/api/comments")
+//    public method returns a list of comment objects
+    public List<Comment> getAllComments() {
+        return repository.findAll();
+    }
+//    get route for single comment
+    @GetMapping("/api/comments/{id}")
+//    public method returns single comment, take in id from url path
+    public Comment getComment(@PathVariable int id) {
+//        returns the comment by id
+        return repository.getById(id);
+    }
 }
