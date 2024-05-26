341beead00f47dfa158c0f32d321c86963238048
mintyry <gitmintyry@gmail.com>
2024-05-25 20:49:36 -0700

    autowired repos in homepage controller

--- src/main/java/com/technews/controller/HomePageController.java
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -1,4 +1,26 @@
 package com.technews.controller;
 
+import com.technews.repository.CommentRepository;
+import com.technews.repository.PostRepository;
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+//Reminder: Autowired informs Spring to scan for objects that need to be instantiated for a class or method to run
+
+//we included repos we created before and mapped them to objects that will be instantiated in this class when necessary.
+@Controller
 public class HomePageController {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
 }
