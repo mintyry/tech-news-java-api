1338f1bd3f7fbe2b02edb0381d5534bdd3e65a17
mintyry <gitmintyry@gmail.com>
2024-05-30 20:00:28 -0700

    made homepage endpoint

--- src/main/java/com/technews/controller/HomePageController.java
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -1,5 +1,6 @@
 package com.technews.controller;
 
+import com.technews.model.Post;
 import com.technews.repository.CommentRepository;
 import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
@@ -14,6 +15,7 @@ import org.springframework.ui.Model;
 //import User model for use
 import com.technews.model.User;
 
+import java.util.List;
 
 
 //Reminder: Autowired informs Spring to scan for objects that need to be instantiated for a class or method to run
@@ -33,6 +35,7 @@ public class HomePageController {
     @Autowired
     CommentRepository commentRepository;
 
+//    login endpoint
     @GetMapping("/login")
 //    Model is from Spring framework; remapped this to "model" variable
     public String login(Model model, HttpServletRequest request) {
@@ -46,6 +49,7 @@ public class HomePageController {
         return "login";
     }
 
+//    logout endpoint
     @GetMapping("/users/logout")
 //    this route checks if session exists; if yes, invalidate session and log user out and redirect to login route
     public String logout(HttpServletRequest request) {
@@ -53,5 +57,38 @@ public class HomePageController {
             request.getSession().invalidate();
         }
         return "redirect:/login";
+    }
+
+//    homepage endpoint
+    @GetMapping("/")
+    public String homepageSetup(Model model, HttpServletRequest request) {
+        User sessionUser = new User();
+// check if user is logged in, if so...
+        if (request.getSession(false) != null) {
+            sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+            model.addAttribute("loggedIn", sessionUser.isLoggedIn());
+        } else {
+            model.addAttribute("loggedIn", false);
+        }
+
+//retrieve post data
+//        make postList variable
+        List<Post> postList = postRepository.findAll();
+//        for loop gets all posts and populates into postList variable
+        for (Post p : postList) {
+            p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+            User user = userRepository.getById(p.getUserId());
+            p.setUserName(user.getUsername());
+        }
+//add the postList and loggedIn details to the user model
+        model.addAttribute("postList", postList);
+        model.addAttribute("loggedIn", sessionUser.isLoggedIn());
+
+        // "point" and "points" attributes refer to upvotes.
+        model.addAttribute("point", "point");
+        model.addAttribute("points", "points");
+
+//        display this data in homepage
+        return "homepage";
     }
 }
