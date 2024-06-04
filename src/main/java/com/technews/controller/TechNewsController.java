c120d20df4a31db7df5d200b674016f91c2f96d6
mintyry <gitmintyry@gmail.com>
2024-06-03 19:42:43 -0700

    POST endpoints in technewscontroller

--- src/main/java/com/technews/controller/TechNewsController.java
+++ src/main/java/com/technews/controller/TechNewsController.java
@@ -1,15 +1,23 @@
 package com.technews.controller;
 
+import com.technews.model.User;
 import com.technews.repository.CommentRepository;
 import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+//import Model class from Spring framework
+import org.springframework.ui.Model;
 
 @Controller
 public class TechNewsController {
-    
+
     @Autowired
     PostRepository postRepository;
 
@@ -21,4 +29,39 @@ public class TechNewsController {
 
     @Autowired
     CommentRepository commentRepository;
+
+    @PostMapping("/users/login")
+    public String login(@ModelAttribute User user, Model model, HttpServletRequest request) throws Exception {
+
+        if ((user.getPassword().equals(null) || user.getPassword().isEmpty()) || (user.getEmail().equals(null) || user.getPassword().isEmpty())) {
+            model.addAttribute("notice", "Email address and password must be populated in order to log in!");
+            return "login";
+        }
+
+        User sessionUser = userRepository.findUserByEmail(user.getEmail());
+
+        try {
+//            if sessionUser is invalid, running .equals() will throw an error
+            if (sessionUser.equals(null)) {
+
+            }
+//            We catch error and notify client that email isnt recognized
+        } catch (NullPointerException e) {
+            model.addAttribute("notice", "Email is not recognized!");
+            return "login";
+        }
+
+//        Validate pw
+        String sessionUserPassword = sessionUser.getPassword();
+        boolean isPasswordValid = BCrypt.checkpw(user.getPassword(), sessionUserPassword);
+        if(isPasswordValid == false) {
+            model.addAttribute("notice", "Password is not valid!");
+            return "login";
+        }
+
+        sessionUser.setLoggedIn(true);
+        request.getSession().setAttribute("SESSION_USER", sessionUser);
+
+        return "redirect:/dashboard";
+    }
 }
